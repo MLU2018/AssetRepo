714b6a4e7db7a58998f6c3b180a416ebf6b170c6
MLU2018 <44643300+MLU2018@users.noreply.github.com>
2019-07-31 11:24:40 -0400

    Update DeployCustomerImageToK8.sh
    Updated the Bearer Token.

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,6 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+#./rancher login https://k8s.eur.ad.sag/v3 --token token-vzcht:b6drnslrqsf2cnwnxr8jxp572zqkj4fl5hzxjq94tgl22qh4xdq2r9
+./rancher login https://k8s.eur.ad.sag/v3 --token token-fj8dx:4wpxfc7fcgmpgnks2q6pgxjrx8kvsm7m2jxhqnfshnbc72m7524c57
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
